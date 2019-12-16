70ff34a89e929521838641ca887a47d890c582a4
icedCoughfee <brentmelton99@yahoo.com>
2019-12-16 03:58:27 -0800

    add fade to PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,6 +5,7 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Fade from "@material-ui/core/Fade";
 import { getPkmnImgId } from "../utility/pokemon";
 import CONSTANTS from "../constants";
 
@@ -20,6 +21,9 @@ const useStyles = makeStyles(theme => ({
     height: 0,
     paddingTop: "56.25%", // 16:9
     backgroundSize: "contain"
+  },
+  hiddenImg: {
+    display: "none"
   }
 }));
 
@@ -30,35 +34,43 @@ function PokemonCard({ pokemon }) {
   const classes = useStyles();
   const [hover, setHover] = useState(false);
   const hoverClass = "MuiPaper-elevation5";
+  const imgUrl = `${CONSTANTS.PKMN_IMG_URL}${getPkmnImgId(pokemon.id)}.png`;
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
-    <Link
-      to={{
-        pathname
-      }}
-      className={classes.cardWrapper}
-    >
-      <Card
-        className={`${classes.card} ${hover ? hoverClass : ""}`}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+    <Fade in={imgLoaded}>
+      <Link
+        to={{
+          pathname
+        }}
+        className={classes.cardWrapper}
       >
-        <CardMedia
-          className={classes.media}
-          image={`${CONSTANTS.PKMN_IMG_URL}${getPkmnImgId(pokemon.id)}.png`}
-          title={name}
+        <img
+          src={imgUrl}
+          onLoad={() => {
+            setImgLoaded(true);
+          }}
+          className={classes.hiddenImg}
         />
-        <CardContent>
-          <Typography
-            variant="h5"
-            color="textSecondary"
-            component="p"
-            align="center"
-          >
-            {name}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Link>
+        <Card
+          className={`${classes.card} ${hover ? hoverClass : ""}`}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <CardMedia className={classes.media} image={imgUrl} title={name} />
+          <CardContent>
+            <Typography
+              variant="h5"
+              color="textSecondary"
+              component="p"
+              align="center"
+            >
+              {name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Link>
+    </Fade>
   );
 }
 
