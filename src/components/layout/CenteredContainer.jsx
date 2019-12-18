eda18a1bf70a3c04eb8874406efb65c2850413ce
icedCoughfee <brentmelton99@yahoo.com>
2019-12-18 14:04:19 -0800

    add margin on centered containers

--- src/components/layout/CenteredContainer.jsx
+++ src/components/layout/CenteredContainer.jsx
@@ -3,12 +3,20 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: "50px auto"
+  }
+}));
 
 function CenteredContainer(props) {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="md">
+      <Container maxWidth="md" className={classes.root}>
         <Typography component="div" style={{ display: "flex" }}>
           {props.children}
         </Typography>
